abab8ca44f3803e7d70d8fdf7cb6dcfbaf342001
Tolulope Oyeniyi <toluxsys@yahoo.ca>
2025-02-24 02:01:11 +0100

    deploy

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   reactStrictMode: false,
   env: {
     URL: 'https://api.autohub.africa/',
-    SECRET_KEY: 'kHbSN.%_&Pf4D}(V9-}X2,?23K0m=U5zGe#7yhLe6(dQtxi%R{92zC1tya31JTB5Lbf*b?h.G;L7#KGML?*rc)H,E?3F%F=N/Hg}',
-    ENV: 'production'
+    SECRET_KEY: '',
+    ENV: ''
   },
   images: {
     remotePatterns: [
