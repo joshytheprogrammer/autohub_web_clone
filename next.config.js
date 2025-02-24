bf682088b3c1d34802c5bdb0af3c0a526d551773
Tolulope Oyeniyi <toluxsys@yahoo.ca>
2025-02-24 02:12:06 +0100

    deploy

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: '',
+        protocol: 'https',
         hostname: '',
         port: '',
         pathname: '/**'
