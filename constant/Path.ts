74f90c41d82abcb07fdcd1cbb20d6de19c6eb958
Hammed <servertstng@gmail.com>
2025-04-07 13:17:04 +0100

    07-04-2025 -1

--- constant/Path.ts
+++ constant/Path.ts
@@ -24,23 +24,14 @@ export const CLOUD_BASE_URL = URL.offline.image_path;
 
 export const USAGE_PATH = 
 {
-    BASE_URL_FRONT : `${DESTINATION}blog-detail/`,
-    IMAGE_SLIDER : `${ASSET_URL}product/`, 
-    PRODUCT_FACE : `${ASSET_URL}product/`,
-    AVATAR : `${ASSET_URL}profile_picture/`,
-    WATER_MARK : `${BASE_IMG_URL}constant/water/`,
-    BLOG_POST : `${ASSET_URL}posts/`,
-    RECEIPT : `${ASSET_URL}receipt/`,
-    DEFAULT_AVATAR : `${ASSET_URL}constant/`,
-
-    CLOUD_BASE_URL_FRONT : `${DESTINATION}autohub/autohub/blog-detail/`,
-    CLOUD_IMAGE_SLIDER : `${CLOUD_BASE_URL}autohub/autohub/product/`, 
-    CLOUD_PRODUCT_FACE : `${CLOUD_BASE_URL}autohub/autohub/product/`,
-    CLOUD_AVATAR : `${CLOUD_BASE_URL}autohub/autohub/profile_picture/`,
-    CLOUD_WATER_MARK : `${CLOUD_BASE_URL}autohub/autohub/constant/water/`,
-    CLOUD_BLOG_POST : `${CLOUD_BASE_URL}autohub/autohub/posts/`,
-    CLOUD_RECEIPT : `${CLOUD_BASE_URL}autohub/autohub/receipt/`,
-    CLOUD_DEFAULT_AVATAR : `${CLOUD_BASE_URL}autohub/autohub/constant/`,
+    CLOUD_BASE_URL_FRONT : `${DESTINATION}blog-detail/`,
+    CLOUD_IMAGE_SLIDER : `${CLOUD_BASE_URL}product/`, 
+    CLOUD_PRODUCT_FACE : `${CLOUD_BASE_URL}product/`,
+    CLOUD_AVATAR : `${CLOUD_BASE_URL}profile_picture/`,
+    CLOUD_WATER_MARK : `${CLOUD_BASE_URL}constant/water/`,
+    CLOUD_BLOG_POST : `${CLOUD_BASE_URL}posts/`,
+    CLOUD_RECEIPT : `${CLOUD_BASE_URL}receipt/`,
+    CLOUD_DEFAULT_AVATAR : `${CLOUD_BASE_URL}constant/`,
 }
 
 
