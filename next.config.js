e6094bffc44196fce6d53a2133f2c64db16f18cb
Hammed <servertstng@gmail.com>
2025-04-09 07:13:23 +0100

    09-04-2025 config-change

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  // output: "export",
   reactStrictMode: false,
   env: {
-    URL: 'http://127.0.0.1:8888',
-    SECRET_KEY: 'kHbSN.%_&Pf4D}(V9-}X2,?23K0m=U5zGe#7yhLe6(dQtxi%R{92zC1tya31JTB5Lbf*b?h.G;L7#KGML?*rc)H,E?3F%F=N/Hg}',
+    URL: 'https://api.autohub.africa',
+    SECRET_KEY: '',
     CLOUD_BASE_URL: 'https://eu2.contabostorage.com/531567a74f5740769e7b8d34e116cadd:autohub/',
-    ENV: 'development'
+    ENV: ''
   },
   images: {
    unoptimized: true,
