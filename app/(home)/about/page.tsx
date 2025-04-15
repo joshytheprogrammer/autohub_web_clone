8bd1a6f2c102d24fd4c097ff61966263ffae077b
Hammed <servertstng@gmail.com>
2025-04-15 10:41:06 +0100

    15-04-2025 About Us Page

--- app/(home)/about/page.tsx
+++ app/(home)/about/page.tsx
@@ -6,13 +6,30 @@ export default function AboutUs()
 
   return (
             <div 
-                  className='container md:grid md:grid-cols-12 justify-between mt-7 mb-4 mx-auto'
+                  className='container w-full d-flex justify-between mt-7 mb-4 mx-auto my-10'
             > 
-                    <h1 
-                        className='font-bold'
+                <div 
+                  className="w-full d-flex"
+                >
+                    <div 
+                        className='font-bold text-2xl mt-10'
                     >
                         About Us
-                    </h1>
+                    </div>
+
+                    <div 
+                      className="text-xl border-shadow py-10"
+                    >
+                        <p>
+                          Autohub Africa is a Nigerian company poised to revolutionize the automobile industry through a turn-key automobile services, ranging from land-vehicles to aviation, maritime, AI and space exploration. 
+                        </p>
+                        <p className="mt-10">
+                          We are building an inter-business community for maximum utilization of automotive products.
+                        </p>
+                    </div>
+                </div>
+
+                <div className="h-[150px] md:h-[350px]"></div>
             </div>
   )
 }
