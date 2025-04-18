4cbfed9e272fd2337ad85dd577e83be465842ad8
Hammed <servertstng@gmail.com>
2025-04-18 09:05:32 +0100

    18-04-2025

--- components/shared/AppPages.tsx
+++ components/shared/AppPages.tsx
@@ -11,38 +11,38 @@ export const AppPages = [
         name: 'Contact Us', 
         goTo: '/contact' 
     },
-    {   
-        name: 'Sell & Buy', 
-        goTo: '/sell-and-buy' 
-    },
-    {   
-        name: 'Build Your Money', 
-        goTo: '/build-your-money' 
-    },
-    {   
-        name: 'Dealership Loan', 
-        goTo: '/dealership-loan' 
-    },
-    {   
-        name: 'Car Loan', 
-        goTo: '/car-loan' 
-    },
+    // {   
+    //     name: 'Sell & Buy', 
+    //     goTo: '/sell-and-buy' 
+    // },
+    // {   
+    //     name: 'Build Your Money', 
+    //     goTo: '/build-your-money' 
+    // },
+    // {   
+    //     name: 'Dealership Loan', 
+    //     goTo: '/dealership-loan' 
+    // },
+    // {   
+    //     name: 'Car Loan', 
+    //     goTo: '/car-loan' 
+    // },
     {   
         name: 'MACEOS Academy', 
         goTo: '/portal' 
     },
-    {   
-        name: 'OAS', 
-        goTo: '/oas' 
-    },
-    {   
-        name: 'Blog', 
-        goTo: '/blog' 
-    },
-    {   
-        name: 'FAQ', 
-        goTo: '/faq' 
-    },
+    // {   
+    //     name: 'OAS', 
+    //     goTo: '/oas' 
+    // },
+    // {   
+    //     name: 'Blog', 
+    //     goTo: '/blog' 
+    // },
+    // {   
+    //     name: 'FAQ', 
+    //     goTo: '/faq' 
+    // },
     // {   
     //     name: 'Login', 
     //     goTo: '/login' 
