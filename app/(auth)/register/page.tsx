156969d2ac8131d70b7450edd3ec572e18eb6c56
Hammed <servertstng@gmail.com>
2025-05-15 15:16:32 +0100

    2

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -19,7 +19,7 @@ export default function Page()
                   className="w-full md:w-6/12 mx-auto my-4 d-flex items-center justify-center p-10 md:-mt-30 mt-5 gap-5"
             >            
                 <div 
-                      className='w-full flex justify-center items-center mb-5 md:mb-20'
+                      className='w-full flex justify-center items-center mb-10 md:mb-20'
                   >
                       <Logo /> 
                 </div>
@@ -32,7 +32,7 @@ export default function Page()
                     <Link className='font-bold ml-2' href={'/register/member'}
                     >
                       <div 
-                            className="border-2 border-green-200 h-[200px] flex md:flex items-center justify-center rounded-xl cursor-pointer bg-green-100 hover:bg-[#23913b] hover:text-white"
+                            className="border-2 border-green-200 h-[160px] md:h-[180px] flex md:flex items-center justify-center rounded-xl cursor-pointer bg-green-100 hover:bg-[#23913b] hover:text-white"
                       >
                           <div 
                                 className='d-flex justify-center items-center mx-5'
@@ -40,7 +40,7 @@ export default function Page()
                                 <div 
                                     className='w-full mx-auto flex justify-center items-center'
                                 >                                            
-                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={.5} stroke="currentColor" className="w-20 h-20">
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={.5} stroke="currentColor" className="w-10 h-10 md:w-15 md:h-15">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                                   </svg>
                                 </div>
@@ -58,7 +58,7 @@ export default function Page()
                       <Link className='font-bold ml-2' href={'/register/dealer'}
                       >
                         <div 
-                              className="border-2 border-green-200 h-[200px] flex md:flex items-center justify-center rounded-xl cursor-pointer bg-green-100 hover:bg-[#23913b] hover:text-white"
+                            className="border-2 border-green-200 h-[160px] md:h-[180px] flex md:flex items-center justify-center rounded-xl cursor-pointer bg-green-100 hover:bg-[#23913b] hover:text-white"
                         >
                           <div 
                                 className='d-flex justify-center items-center mx-5'
@@ -66,7 +66,7 @@ export default function Page()
                                 <div 
                                     className='w-full mx-auto flex justify-center items-center'
                                 >                                            
-                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={.5} stroke="currentColor" className="w-20 h-20">
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={.5} stroke="currentColor" className="w-10 h-10 md:w-15 md:h-15">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
                                   </svg>
                                 </div>
@@ -84,7 +84,7 @@ export default function Page()
                       <Link className='font-bold ml-2' href={'/register/affiliate'}
                       >
                         <div 
-                              className="border-2 border-green-200 h-[200px] flex md:flex items-center justify-center rounded-xl cursor-pointer bg-green-100 hover:bg-[#23913b] hover:text-white"
+                            className="border-2 border-green-200 h-[160px] md:h-[180px] flex md:flex items-center justify-center rounded-xl cursor-pointer bg-green-100 hover:bg-[#23913b] hover:text-white"
                         >
                             <div 
                                   className='d-flex justify-center items-center mx-5'
@@ -92,7 +92,7 @@ export default function Page()
                                   <div 
                                       className='w-full mx-auto flex justify-center items-center'
                                   >   
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={.5} stroke="currentColor" className="w-20 h-20">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={.5} stroke="currentColor" className="w-10 h-10 md:w-15 md:h-15">
                                           <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                                     </svg>
                                   </div>
